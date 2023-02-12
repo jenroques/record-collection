d3fa7b81b8594f00f15e70b4d829f1dafc6b5c31
Jennifer <jenniferroques@gmail.com>
2023-02-12 16:54:17 -0600

    Added Authorize and Rescue From Errors to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorize
 
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }
+
+  def authorize
+    return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
   end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Not Found" }, status: :not_found
+  end
+
+  def render_invalid_record(invalid)
+    render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
