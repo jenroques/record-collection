cb672ea1a89004ba04dffea6875dabf47ef4a139
Jennifer <jenniferroques@gmail.com>
2023-02-12 18:44:06 -0600

    Change "name" to "title"

--- db/migrate/20230211233101_create_records.rb
+++ db/migrate/20230211233101_create_records.rb
@@ -1,7 +1,7 @@
 class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
-      t.string :name
+      t.string :title
       t.string :image_url
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :collection, null: false, foreign_key: true
