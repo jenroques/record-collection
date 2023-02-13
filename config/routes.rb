5caadffdd742271be1447d5772e5bca1d4324246
Jennifer <jenniferroques@gmail.com>
2023-02-12 18:43:28 -0600

    created first draft routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show, :create]
+  resources :artist, only: [:index, :show, :create, :update, :destroy]
+  resources :records, only: [:index, :show, :create, :update, :destroy]
+  resources :collections, only: [:index, :show, :create, :update, :destroy]
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
 
-  get '/hello', to: 'application#hello_world'
+
 
   get '*path',
   to: 'fallback#index',
