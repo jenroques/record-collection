99c287c3779ebda007f67895c60180eb9e4e5df6
Jennifer <jenniferroques@gmail.com>
2023-02-12 18:42:58 -0600

    Updated methods

---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,42 @@
+class RecordController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  skip_before_action :authorize, only: [:show]
+  wrap_parameters :record, include: [:title, artist_ids: []]
+
+  def index
+    @records = Record.all
+    render json: @records
+  end
+
+  def create
+    @record = Record.create(record_params)
+    render json: @record, status: :created
+  end
+
+  def update
+    @record = find_record
+    @record.update(update_record_params)
+    render json: @record, status: :accepted
+  end
+
+  def destroy
+    @record = find_record
+    @record
+    @record.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_record
+    Record.find(params[:id])
+  end
+
+  def record_params
+    params.permit(:title, :image_url, :user_id, :collection_id, :artist_id)
+  end
+
+  def update_record_params
+    params.permit(:title, :image_url, :collection_id)
+
