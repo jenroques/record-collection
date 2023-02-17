9381285e6d7c438bc1dad3c3cd91b78442a1bd6b
Jennifer <jenniferroques@gmail.com>
2023-02-17 17:31:54 -0600

    Added rescue_from to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   before_action :authorize
 
 
