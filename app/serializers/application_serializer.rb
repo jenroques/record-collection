0ea442f3fc460fddf3e892b5601bb9509138efcd
Jennifer <jenniferroques@gmail.com>
2023-02-20 12:17:47 -0600

    Errors

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,6 +1,7 @@
 class ApplicationSerializer < ActiveModel::Serializer
   include ActiveRecord::Associations
   include ActiveRecord::ModelSchema
+  include ActiveRecord::AttributeMethods
   include ActiveRecord::AutosaveAssociation
   include ActiveRecord::Callbacks
   include ActiveModel::Validations
