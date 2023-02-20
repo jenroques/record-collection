1199bdda1e51ded93f32fcad3eef8d33d82c31ea
Jennifer <jenniferroques@gmail.com>
2023-02-20 11:50:28 -0600

    Trying to fix Artist Serializer Errors

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,7 +1,7 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  include ActiveRecord::Associations
-  include ActiveRecord::ModelSchema
-  include ActiveRecord::AutosaveAssociation
-  include ActiveRecord::Callbacks
-  include ActiveModel::Validations
+  # include ActiveRecord::Associations
+  # include ActiveRecord::ModelSchema
+  # include ActiveRecord::AutosaveAssociation
+  # include ActiveRecord::Callbacks
+  # include ActiveModel::Validations
 end
--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ApplicationSerializer
   attributes :id, :name, :image_url, :record_id
 
-  has_and_belongs_to_many :records
+  # has_and_belongs_to_many :records
 end
