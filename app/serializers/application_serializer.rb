060f98cc270eacd3647b2c9c1b33bbcd0d1fba08
Jennifer <jenniferroques@gmail.com>
2023-02-20 11:36:34 -0600

    Include ActiveRecord::Callbacks

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -2,4 +2,5 @@ class ApplicationSerializer < ActiveModel::Serializer
   include ActiveRecord::Associations
   include ActiveRecord::ModelSchema
   include ActiveRecord::AutosaveAssociation
+  include ActiveRecord::Callbacks
 end
