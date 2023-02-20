e7d6762fd304ab329787c0d5ea665802c51d429c
Jennifer <jenniferroques@gmail.com>
2023-02-20 11:56:49 -0600

    changed to plural, updated association

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,3 @@
 class Artist < ApplicationRecord
-  has_and_belongs_to_many :record
+  has_and_belongs_to_many :records
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ApplicationRecord
   belongs_to :user
   belongs_to :collection
-  has_and_belongs_to_many :artist
+  has_and_belongs_to_many :artists
 end
