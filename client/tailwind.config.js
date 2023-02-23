fae9bc19639c658549c437a895a99234f8bbd415
Jennifer <jenniferroques@gmail.com>
2023-02-23 14:59:38 -0600

    Updated tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    '.client/src/**/*.{html,js}',
+    '.client/public/index.html'
+  ],
   theme: {
     extend: {},
   },
