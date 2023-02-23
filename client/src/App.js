448009b73c7785138a1f5168dcc4893f0197e2cd
Jennifer <jenniferroques@gmail.com>
2023-02-23 14:11:52 -0600

    Added BrowserRouter and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Utils/store";
+
+import Artists from "./Artists/Artists";
+import Collections from "./Collections/Collections";
+import CollectionDetail from './Collections/CollectionDetail';
+import Records from './Records/Records';
+import RecordDetail from './Records/RecordDetail';
+import UserProfile from './User/UserProfile';
+import Login from './User/Login';
+import Logout from './User/Logout';
+import Home from './Utils/Home';
+import Nav from './Utils/Nav';
+
+
 
 function App() {
-
-
   return (
-    <BrowserRouter>
-      <div className="App">
+    <Provider store={store}>
+      <BrowserRouter>
+        <Nav />
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Welcome</h1>
-          </Route>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/artists" component={Artists} />
+          <Route exact path="/collections" component={Collections} />
+          <Route exact path={`/collection/${id}`} component={CollectionDetail} />
+          <Route exact path="/records" component={Records} />
+          <Route exact path={`/records/${id}`} component={RecordDetail} />
+          <Route exact path="/me" component={UserProfile} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
         </Switch>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
