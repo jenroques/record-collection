2017eca724b80317539bda3e561867a5e78f0f8f
Jennifer <jenniferroques@gmail.com>
2023-02-24 14:52:00 -0600

    Working through design

--- client/src/Utils/Nav.js
+++ client/src/Utils/Nav.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const Nav = () => {
+function Nav() {
     return (
-        <div>Nav</div>
-    )
+        <nav>
+            <ul>
+                <li>
+                    <NavLink exact to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/records">Records</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/collections">Collections</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/artists">Artists</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/search">Search</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/profile">Profile</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/logout">Logout</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
-export default Nav
+export default Nav;
+
+
