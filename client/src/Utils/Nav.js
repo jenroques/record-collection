2cc3cc1472d31c887b2a14fcfadadbff1d099dbf
Jennifer <jenniferroques@gmail.com>
2023-02-27 10:51:21 -0600

    Created Nav.js design and functionality

--- client/src/Utils/Nav.js
+++ client/src/Utils/Nav.js
@@ -1,33 +1,131 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-function Nav() {
+import { AppBar, Box, Button, Container, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Logo from '../Assets/logo.png';
+import RecordMini from '../Assets/vinyl.png';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useDispatch } from "react-redux";
+import { logout } from "../Utils/store"
+import { useHistory } from 'react-router-dom';
+
+
+const Nav = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+
+    const handleLogout = () => {
+        console.log("Trying to logout")
+        dispatch(logout())
+            .then(() => {
+                history.push('/login');
+            });
+    }
+
+    const pages = [
+        { name: "Records", path: "/records" },
+        { name: "Artists", path: "/artists" },
+        { name: "Collections", path: "/collections" },
+    ];
+
+    const settings = [
+        { name: "Profile", path: "/me" },
+        { name: "Logout", path: "/logout", onClick: handleLogout }
+    ];
+
+
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink exact to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/records">Records</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/collections">Collections</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/artists">Artists</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/search">Search</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/profile">Profile</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/logout">Logout</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <AppBar position="static" sx={{ backgroundColor: '#c3d6c8' }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Link to="/" sx={{ flexGrow: 0 }}>
+                        <img src={Logo} alt="Logo" width="100" height="100" />
+                    </Link>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, marginLeft: '1rem' }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                onClick={handleCloseNavMenu}
+                                size="large"
+                                sx={{ my: 2, color: 'black', display: 'block' }}
+                                component={Link}
+                                to={page.path}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <MenuIcon fontSize="large" />
+                                <img src={RecordMini} alt="Vinyl Icon" style={{ width: '40px', height: '40px' }} />
+
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem
+                                    key={setting.name}
+                                    onClick={
+                                        setting.name === 'Logout'
+                                            ? () => {
+                                                handleCloseUserMenu();
+                                                handleLogout();
+                                            }
+                                            : handleCloseUserMenu
+                                    }
+                                    component={Link}
+                                    to={setting.path}
+                                >
+                                    <Typography sx={{ color: '#000000', fontWeight: 'bold' }} textAlign="center">
+                                        {setting.name}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
