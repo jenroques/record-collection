c82ef2cb196c89d40b02e02c106016d1499f99ef
Jennifer <jenniferroques@gmail.com>
2023-02-27 13:35:27 -0600

    Working on routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import Artists from "./Artists/Artists";
@@ -8,7 +8,6 @@ import Records from './Records/Records';
 import UserProfile from './User/UserProfile';
 import SignUp from './User/SignUp';
 import Login from './User/Login';
-import Logout from './User/Logout';
 import Home from './Utils/Home';
 import Nav from './Utils/Nav';
 import { authenticate } from "./Utils/store";
@@ -30,42 +29,19 @@ function App() {
     }
   }, [dispatch]);
 
-  function LoginPage({ isLoggedIn }) {
-    const history = useHistory();
-
-    useEffect(() => {
-      if (isLoggedIn) {
-        history.push("/");
-      }
-    }, [isLoggedIn, history]);
-
-    return <Login />;
-  }
-
-  function SignUpPage({ isLoggedIn }) {
-    const history = useHistory();
-
-    useEffect(() => {
-      if (isLoggedIn) {
-        history.push("/");
-      }
-    }, [isLoggedIn, history]);
-
-    return <SignUp />;
-  }
 
   return (
     <BrowserRouter>
       {isLoggedIn && <Nav />}
       <Switch>
-        <Route path="/welcome" component={Home} />
-        <Route path="/artists" component={Artists} />
-        <Route path="/collections" component={Collections} />
-        <Route path="/records" component={Records} />
-        <Route path="/me" component={UserProfile} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/login" render={() => <Login isLoggedIn={isLoggedIn} />} />
-        <Route path="/signup" render={() => <SignUp isLoggedIn={isLoggedIn} />} />
+        <Route path="/" exact component={Login} />
+        <Route path="/welcome" exact component={Home} />
+        <Route path="/artists" exact component={Artists} />
+        <Route path="/collections" exact component={Collections} />
+        <Route path="/records" exact component={Records} />
+        <Route path="/me" exact component={UserProfile} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={SignUp} />
       </Switch>
     </BrowserRouter>
   );
