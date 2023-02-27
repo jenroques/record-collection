e2b266aa6561257c5d6118fbb539e4566ac4fd0f
Jennifer <jenniferroques@gmail.com>
2023-02-27 11:12:51 -0600

    Navigation updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,73 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./Utils/store";
+import React, { useEffect } from "react";
+import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import Artists from "./Artists/Artists";
 import Collections from "./Collections/Collections";
 import Records from './Records/Records';
 import UserProfile from './User/UserProfile';
+import SignUp from './User/SignUp';
 import Login from './User/Login';
 import Logout from './User/Logout';
 import Home from './Utils/Home';
 import Nav from './Utils/Nav';
-
-
+import { authenticate } from "./Utils/store";
 
 function App() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.session.isLoggedIn);
+
+  useEffect(() => {
+    const sessionId = localStorage.getItem("sessionId");
+    if (sessionId) {
+      dispatch(authenticate())
+        .then(() => {
+          console.log("Successfully authenticated");
+        })
+        .catch((error) => {
+          console.log("Authentication failed:", error.message);
+        });
+    }
+  }, [dispatch]);
+
+  function LoginPage({ isLoggedIn }) {
+    const history = useHistory();
+
+    useEffect(() => {
+      if (isLoggedIn) {
+        history.push("/");
+      }
+    }, [isLoggedIn, history]);
+
+    return <Login />;
+  }
+
+  function SignUpPage({ isLoggedIn }) {
+    const history = useHistory();
+
+    useEffect(() => {
+      if (isLoggedIn) {
+        history.push("/");
+      }
+    }, [isLoggedIn, history]);
+
+    return <SignUp />;
+  }
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/artists" component={Artists} />
-          <Route exact path="/collections" component={Collections} />
-          <Route exact path="/records" component={Records} />
-          <Route exact path="/me" component={UserProfile} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/logout" component={Logout} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      {isLoggedIn && <Nav />}
+      <Switch>
+        <Route path="/welcome" component={Home} />
+        <Route path="/artists" component={Artists} />
+        <Route path="/collections" component={Collections} />
+        <Route path="/records" component={Records} />
+        <Route path="/me" component={UserProfile} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/login" render={() => <Login isLoggedIn={isLoggedIn} />} />
+        <Route path="/signup" render={() => <SignUp isLoggedIn={isLoggedIn} />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
