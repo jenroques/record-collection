b9c867e9e7b3803c763d87ce6d08ff5e4392d691
Jennifer <jenniferroques@gmail.com>
2023-03-01 17:33:12 -0600

    Building out

--- client/src/Utils/Home.js
+++ client/src/Utils/Home.js
@@ -1,14 +1,83 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect, useDispatch } from 'react-redux'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Box, Toolbar, Paper, Grid, Container, Typography } from '@mui/material';
+
+import { SideNav } from './SideNav';
+
+const theme = createTheme();
 
 
-const Home = () => {
-
+export const Home = (props) => {
     console.log("ET Phone Home?")
 
 
     return (
-        <div>Home</div>
-    )
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <SideNav />
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[100]
+                                : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: '100vh',
+                        overflow: 'auto',
+                    }}
+                >
+                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                        <Grid container spacing={3}>
+                            {/* Chart */}
+                            <Grid item xs={12} md={8} lg={9}>
+                                <Paper
+                                    sx={{
+                                        p: 2,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        height: 240,
+                                    }}
+                                >
+                                    <Typography> Record Stats </Typography>
+                                </Paper>
+                            </Grid>
+                            {/* Recent Deposits */}
+                            <Grid item xs={12} md={4} lg={3}>
+                                <Paper
+                                    sx={{
+                                        p: 2,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        height: 240,
+                                    }}
+                                >
+                                    <Typography> Artist Stats </Typography>
+                                </Paper>
+                            </Grid>
+                            {/* Recent Orders */}
+                            <Grid item xs={12}>
+                                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                                    <Typography> Collection Stats </Typography>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
 }
 
-export default Home
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
