5d712eb3ece3889f5d7d92c5a8f44cf0820ba639
Jennifer <jenniferroques@gmail.com>
2023-03-03 08:02:47 -0600

    Updated clearError WIP

--- client/src/User/Login.js
+++ client/src/User/Login.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect, useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom';
-import { CssVarsProvider } from '@mui/joy/styles';
-import { Alert, Sheet, Typography, FormControl, FormLabel, Input, Button, Link } from '@mui/joy';
-import WarningIcon from '@mui/icons-material/Warning';
+import { Alert, Button, CssBaseline, TextField, Link, Paper, Box, Grid, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import LogoVid from '../Assets/logovid.mp4';
 
-import { login } from '../Action/actions';
+import { login, setIsCreated, clearError } from '../Action/actions';
+
+const theme = createTheme();
 
 export const Login = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const history = useHistory();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(clearError());
+    }, [dispatch]);
 
     const handleUsernameChange = (e) => {
         setUsername(e.target.value);
@@ -28,6 +33,7 @@ export const Login = (props) => {
         const credentials = { username, password };
         dispatch(login(credentials))
             .then(() => {
+                dispatch(setIsCreated(false));
                 history.push("/welcome");
                 console.log("Successfully logged in")
                 setUsername('');
@@ -40,79 +46,98 @@ export const Login = (props) => {
 
 
     console.log("Is Logged in? ", props.isLoggedIn)
+    console.log("Is created? ", props.isCreated)
 
     return (
-        <div style={{ position: 'relative', height: '100vh' }}>
-            <CssVarsProvider>
-                <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', overflow: 'hidden' }}>
-                    <div style={{ position: 'relative', width: '100%', height: '100%', zIndex: -2, backgroundColor: '#c3d6c8' }}>
-                        <video autoPlay muted
-                            style={{ position: 'absolute', top: '18%', left: '50%', transform: 'translate(-50%, -50%)', minWidth: '10%', minHeight: '10%', maxWidth: '35%', maxHeight: '35%', width: 'auto', height: 'auto', zIndex: -1, borderRadius: 'sm' }}>
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '100vh', backgroundColor: '#f6f8f9' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                >
+                    <Box
+                        sx={{
+                            mt: 20,
+                            ml: 20,
+
+                        }}
+                    >
+                        <video
+                            autoPlay
+                            muted
+                            style={{
+                                position: 'absolute',
+                                left: '0',
+                                minWidth: '30%',
+                                minHeight: '30%',
+                                width: '60%',
+                                height: '60%',
+                                zIndex: '0',
+                            }}
+                        >
                             <source src={LogoVid} type="video/mp4" />
                         </video>
-                    </div>
-                </div>
-                <main style={{ backgroundColor: 'transparent', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <div style={{
-                        position: 'absolute', top: '49%', left: '47%', transform: 'translate(-50%, -50%)', width: '300px'
-                    }}>
-                        <Sheet
-                            sx={{
-                                width: 400,
-                                mx: 'auto', // margin left & right
-                                my: 4, // margin top & botom
-                                py: 1, // padding top & bottom
-                                px: 2, // padding left & right
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: 2,
-                                borderRadius: 'sm',
-                                boxShadow: 'md',
-                                background: '#F9FDFA',
-                                textAlign: 'center'
-                            }}
-                            variant="outlined"
-                        >
-                            <div>
-                                <Typography level="h4" component="h1">
-                                    <b>Welcome!</b>
-                                </Typography>
-                                <Typography level="body2">Sign in to continue.</Typography>
-                            </div>
-                            <FormControl>
-                                <FormLabel>Email</FormLabel>
-                                <Input
-                                    name="username"
-                                    type="username"
-                                    placeholder="User Name"
-                                    required
-                                    value={username}
-                                    onChange={handleUsernameChange}
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Password</FormLabel>
-                                <Input
-                                    name="password"
-                                    type="password"
-                                    placeholder="password"
-                                    required
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                />
-                            </FormControl>
-
-                            <Button sx={{ mt: 1, backgroundColor: '#c3d6c8', color: 'black' }} onClick={handleSubmit}> Log in </Button>
-                            <Typography
-                                endDecorator={<Link href="/signup" sx={{ color: "#606761" }}>Sign up</Link>}
-                                fontSize="sm"
-                                sx={{ alignSelf: 'center' }}
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            mt: 40,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography component="h1" variant="h4">
+                            Welcome Record Collector
+                        </Typography>
+                        <Typography component="h2" variant="h5">
+                            Sign in to your Collection:
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                autoFocus
+                                onChange={handleUsernameChange}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                onChange={handlePasswordChange}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2, color: 'black', backgroundColor: '#c3d6c8' }}
                             >
-                                Don&apos;t have an account?
-                            </Typography>
-                            {props.error && props.error &&
+                                Sign In
+                            </Button>
+                            <Grid container>
+
+                                <Grid item>
+                                    <Link href="/signup" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                            {props.error &&
                                 <Alert
-                                    startDecorator={<WarningIcon sx={{ mx: 0.5 }} />}
                                     variant="outlined"
                                     color="warning"
                                 >
@@ -121,22 +146,33 @@ export const Login = (props) => {
                                     </Typography>
                                 </Alert>
                             }
-                        </Sheet>
-
-                    </div>
-                </main>
-            </CssVarsProvider>
-        </div >
+                            {props.isCreated && (
+                                <Alert
+                                    variant="outlined"
+                                    severity="success"
+                                    sx={{ mb: 2 }}
+                                >
+                                    Account Created, please login with your new account information.
+                                </Alert>
+                            )}
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider >
     );
 }
 
 const mapStateToProps = (state) => ({
     error: state.session.error,
-    isLoggedIn: state.session.isLoggedIn
+    isLoggedIn: state.session.isLoggedIn,
+    isCreated: state.user.isCreated,
 })
 
 const mapDispatchToProps = (dispatch) => ({
     login: (credentials) => dispatch(login(credentials)),
+    setIsCreated: () => dispatch(setIsCreated()),
+    clearError: () => dispatch(clearError()),
 });
 
 
