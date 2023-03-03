d48e1c9fb3a0ffd4d6027d2a6e0e8f6d0e8ddc10
Jennifer <jenniferroques@gmail.com>
2023-03-03 08:02:25 -0600

    Updated reducers

--- client/src/Slice/artistSlice.js
+++ client/src/Slice/artistSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchArtists, fetchCollectionById, deleteArtist, deleteArtistFromRecord } from "../Action/actions";
+import { fetchArtists, fetchArtistById, deleteArtist, deleteArtistFromRecord, editArtist } from "../Action/actions";
 
 export const initialState = {
     artists: [],
@@ -11,41 +11,65 @@ export const artistSlice = createSlice({
     reducers: {
         setArtistId(state, action) {
             const { id, artist } = action.payload;
-            const index = state.findIndex((a) => a.id === id);
-            state[index] = { ...artist, id };
+            const index = state.artists.findIndex((a) => a.id === id);
+            if (index !== -1) {
+                state.artists[index] = { ...artist, id };
+            }
+            return state;
         },
         addArtist: (state, action) => {
-            state.push(action.payload);
+            state.artists.push(action.payload);
         },
         deleteArtist: (state, action) => {
-            return state.filter((collection) => collection.id !== action.payload.id);
+            return {
+                ...state,
+                artists: state.artists.filter((collection) => collection.id !== action.payload.id),
+            };
+        },
+        editArtist: (state, action) => {
+            const updatedArtist = action.payload;
+            const index = state.artists.findIndex((artist) => artist.id === updatedArtist.id);
+            if (index !== -1) {
+                state.artists[index] = updatedArtist
+            }
+            return state;
         },
     },
     extraReducers: (builder) => {
         builder.addCase(fetchArtists.fulfilled, (state, action) => {
-            return action.payload.map((artist) => {
+            console.log('fetchArtists.fulfilled:', action.payload);
+            const artists = action.payload.map((artist) => {
                 const { id, ...rest } = artist;
                 return { ...rest, id };
             });
+            console.log('artists:', artists);
+            return { ...state, artists };
         });
-        builder.addCase(fetchCollectionById.fulfilled, (state, action) => {
+        builder.addCase(fetchArtistById.fulfilled, (state, action) => {
             const { id } = action.payload;
-            const index = state.findIndex((collection) => collection.id === id);
+            const index = state.artists.findIndex((artist) => artist.id === id);
             if (index !== -1) {
-                state[index] = action.payload;
+                state.artists[index] = action.payload;
             } else {
-                state.push(action.payload);
+                state.artists.push(action.payload);
             }
         });
         builder.addCase(deleteArtist.fulfilled, (state, action) => {
             const artistId = action.payload.id;
-            return state.filter((a) => a.id !== artistId);
+            return {
+                ...state,
+                artists: state.artists.filter((a) => a.id !== artistId),
+            };
         });
         builder.addCase(deleteArtistFromRecord.fulfilled, (state, action) => {
             const artistId = action.payload.id;
-            return state.filter((artist) => artist.id !== artistId);
+            return {
+                ...state,
+                artists: state.artists.filter((artist) => artist.id !== artistId),
+            };
         });
     },
 });
 
-export default artistSlice.reducer
+export const { setArtistId, addArtist } = artistSlice.actions;
+export default artistSlice.reducer;
