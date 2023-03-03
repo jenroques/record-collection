b4a928d67cc32af3b02805f0bf37c6ac5296436b
Jennifer <jenniferroques@gmail.com>
2023-03-03 08:02:03 -0600

    building out collections display

--- client/src/Collections/Collections.js
+++ client/src/Collections/Collections.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { connect, useDispatch } from 'react-redux'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Box, Toolbar, Paper, Grid, Container, Typography } from '@mui/material';
 
-const Collections = () => {
+import SideNav from '../Utils/SideNav';
+
+const theme = createTheme();
+
+
+
+export const Collections = (props) => {
     return (
-        <div>Collections</div>
-    )
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <SideNav />
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[100]
+                                : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: '100vh',
+                        overflow: 'auto',
+                    }}
+                >
+                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                        <Grid container spacing={3}>
+
+                            <Grid item xs={12}>
+                                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                                    <Typography> Collections </Typography>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
+
 }
 
-export default Collections
+
+export default Collections;
