ca04ffa619913db74a41ef36427d911527fb6423
Jennifer <jenniferroques@gmail.com>
2023-03-05 16:36:36 -0600

    Added functionality

--- client/src/Records/RecordDetail.js
+++ client/src/Records/RecordDetail.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CardMedia, Card, CardContent, Divider, CssBaseline, Box, Grid, Container, Typography } from '@mui/material';
+import { fetchRecordById } from '../Action/actions';
 
-const RecordDetail = () => {
+
+
+const theme = createTheme();
+
+export const RecordDetail = ({ recordId }) => {
+    const dispatch = useDispatch();
+    const currentRecord = useSelector((state) => state.records.currentRecord);
+    const [recordDetail, setRecordDetail] = useState([])
+    console.log(recordId)
+
+    useEffect(() => {
+        dispatch(fetchRecordById(recordId));
+    }, [dispatch, recordId]);
+
+    useEffect(() => {
+        setRecordDetail(currentRecord);
+    }, [currentRecord, setRecordDetail]);
+
+    console.log(recordDetail);
+
     return (
-        <div>RecordDetail</div>
-    )
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <Grid container spacing={4} sx={{ width: 500 }}>
+                    <Grid item key={recordId} >
+                        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                            <CardMedia component="img" image={recordDetail.image_url} width="50%" height="50%" alt="record_image" />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {recordDetail.title}
+                                </Typography>
+                                <Typography>
+                                    {recordDetail && recordDetail.artists && recordDetail.artists[0].name}
+                                </Typography>
+                                <Divider />
+                                <Typography gutterBottom variant="h6" component="h6" sx={{ mt: 2 }}>
+                                    Collections:
+                                </Typography>
+                                <Typography>{recordDetail.collection && recordDetail.collection.name}</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Box>
+        </ThemeProvider>
+    );
 }
-
-export default RecordDetail
+export default RecordDetail;
