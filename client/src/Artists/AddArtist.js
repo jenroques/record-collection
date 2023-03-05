66fb2e29aec99bdbb801072c4af9ec3ca016f66d
Jennifer <jenniferroques@gmail.com>
2023-03-05 12:34:19 -0600

    Building functionality

--- client/src/Artists/AddArtist.js
+++ client/src/Artists/AddArtist.js
@@ -1,14 +1,80 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import {
+    createArtist,
+} from "../Action/actions";
 
-export const AddArtist = (props) => {
+import {
+    TextField,
+    Button,
+    Grid,
+} from "@mui/material";
+
+const AddArtist = ({ setIsEdited, isEdited, handleCloseAddArtist }) => {
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleImageChange = (event) => {
+        setImageUrl(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(createArtist({ name, image_url: imageUrl }));
+        setIsEdited(!isEdited)
+        setName("");
+        setImageUrl("");
+        handleCloseAddArtist();
+    };
+
     return (
-        <div>AddArtist</div>
-    )
-}
+        <form onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="name"
+                        label="Name"
+                        variant="standard"
+                        fullWidth
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="image_url"
+                        label="Image URL"
+                        variant="standard"
+                        fullWidth
+                        value={imageUrl}
+                        onChange={handleImageChange}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
 
-const mapStateToProps = (state) => ({})
+    );
+};
+const mapStateToProps = (state, ownProps) => {
+    const artists = state.artists.artists
+    return { artists };
+};
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = (dispatch) => ({
+    createArtist: (artist) => dispatch(createArtist(artist)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddArtist)
