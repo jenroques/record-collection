18239e05d9485fbe6e6ee51a3d22f3767e0d60c5
Jennifer <jenniferroques@gmail.com>
2023-03-06 14:09:06 -0600

    Updated name to Create Record

---
+++ client/src/Records/CreateRecord.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import {
+    createRecord,
+} from "../Action/actions";
+
+import {
+    TextField,
+    Button,
+    Grid,
+    Typography,
+    FormControl,
+    Select,
+    MenuItem
+} from "@mui/material";
+
+const AddRecord = ({ setIsEdited, isEdited, handleCloseAddRecord }) => {
+    const dispatch = useDispatch();
+    const collections = useSelector((state) => state.collections.collections);
+    const currentUser = useSelector((state) => state.session.currentUser)
+    const [title, setTitle] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [collectionId, setCollectionId] = useState(0);
+
+    console.log(currentUser.user.id);
+    console.log(title)
+    console.log("imageUrl:", imageUrl)
+    console.log(collectionId)
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleImageChange = (e) => {
+        setImageUrl(e.target.value);
+    }
+
+    const handleCollectionChange = (e) => {
+        setCollectionId(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("dispatching createRecord action");
+        console.log("imageUrl:", imageUrl)
+        console.log("currentUser.id", currentUser.user.id)
+        dispatch(createRecord({ title, image_url: imageUrl, user_id: currentUser.user.id, collection_id: collectionId }));
+        setIsEdited(!isEdited)
+        setTitle("");
+        setImageUrl("");
+        handleCloseAddRecord();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="title"
+                        label="Title"
+                        variant="standard"
+                        fullWidth
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="image_url"
+                        label="Image URL"
+                        variant="standard"
+                        fullWidth
+                        value={imageUrl}
+                        onChange={handleImageChange}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography>Select a Collection for this Record</Typography>
+                    <FormControl>
+                        <Select value={collectionId} onChange={handleCollectionChange}>
+                            {collections.map((collection) => {
+                                return (
+                                    <MenuItem key={collection.id} value={collection.id} collection={collection.title}>
+                                        {collection.name}
+                                    </MenuItem>
+                                );
+                            })}
+                        </Select>
+                    </FormControl>
+                    <div>
+                        <Button type="submit">
+                            Add Record
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </form>
+
+    );
+};
+const mapStateToProps = (state) => {
+    const collections = state.collections.collections
+    return { collections };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    createRecord: () => dispatch(createRecord()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddRecord)
